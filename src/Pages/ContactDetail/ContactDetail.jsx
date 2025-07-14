a763b4657cc75d6f77697339198693e8a487ffad
Muhammad ayub <muhammadayubmrk@gmail.com>
2025-07-14 22:44:32 +0500

    adding contextapi remains

--- src/Pages/ContactDetail/ContactDetail.jsx
+++ src/Pages/ContactDetail/ContactDetail.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import './ContactDetail.css'
+import { useParams } from 'react-router-dom'
 const ContactDetail = () => {
+  const url = useParams()
   return (
     <div className='Contact-detail'>
-        <h1>Fuck you</h1>
+        <h1>{url.contactId}</h1>
     </div>
   )
 }
